84dee0974fdb970d2c8fbc28382bd72a0896b999
Derek Mohr <derek@mightyinthemidwest.com>
2020-11-26 20:37:17 -0500

    Update general config

--- config/general.php
+++ config/general.php
@@ -11,57 +11,73 @@
 use craft\helpers\App;
 
 return [
-    // Global settings
-    '*' => [
-        // Default Week Start Day (0 = Sunday, 1 = Monday...)
-        'defaultWeekStartDay' => 1,
+	// Global settings
+	'*' => [
+		// https://docs.craftcms.com/v3/config/config-settings.html#aliases
+		'aliases' => [
+			'@env'     => getenv('ENVIRONMENT'),
+			'@siteUrl' => getenv('SITE_URL'),
+		],
+		// Control panel trigger word
+		'cpTrigger' => 'admin',
+		// https://docs.craftcms.com/v3/config/config-settings.html#defaultsearchtermoptions
+		'defaultSearchTermOptions' => [
+			'subLeft'  => true,
+			'subRight' => true,
+		],
+		// Default Week Start Day (0 = Sunday, 1 = Monday...)
+		'defaultWeekStartDay' => 0,
+		// https://docs.craftcms.com/v3/config/config-settings.html#generatetransformsbeforepageload
+		'generateTransformsBeforePageLoad' => true,
+		// Whether generated URLs should omit "index.php"
+		'omitScriptNameInUrls' => true,
+		// The secure key Craft will use for hashing and encrypting data
+		'securityKey' => App::env('SECURITY_KEY'),
+		// https://docs.craftcms.com/v3/config/config-settings.html#sendpoweredbyheader
+		'sendPoweredByHeader' => false,
+		// https://docs.craftcms.com/v3/config/config-settings.html#sitename
+		'siteName' => getenv('SITE_NAME'),
+		// https://docs.craftcms.com/v3/config/config-settings.html#siteurl
+		'siteUrl' => getenv('SITE_URL'),
+		// https://docs.craftcms.com/v3/config/config-settings.html#timezone
+		'timezone' => getenv('SITE_TIMEZONE') ?: 'America/Detroit',
+	],
 
-        // Whether generated URLs should omit "index.php"
-        'omitScriptNameInUrls' => true,
+	// Local environment settings
+	'local' => [
+		// https://craftcms.com/docs/3.x/config/config-settings.html#backuponupdate
+		'backupOnUpdate' => false,
+		// Dev Mode (see https://craftcms.com/guides/what-dev-mode-does)
+		'devMode' => true,
+		// Prevent crawlers from indexing pages and following links
+		'disallowRobots' => true,
+		// https://craftcms.com/docs/3.x/config/config-settings.html#enabletemplatecaching
+		'enableTemplateCaching' => false,
+	],
 
-        // Control panel trigger word
-        'cpTrigger' => 'admin',
+	// Dev environment settings
+	'dev' => [
+		// Dev Mode (see https://craftcms.com/guides/what-dev-mode-does)
+		'devMode' => true,
+		// Prevent crawlers from indexing pages and following links
+		'disallowRobots' => true,
+	],
 
-        // The secure key Craft will use for hashing and encrypting data
-        'securityKey' => App::env('SECURITY_KEY'),
-    ],
+	// Staging environment settings
+	'staging' => [
+		// Set this to `false` to prevent administrative changes from being made on Staging
+		'allowAdminChanges' => true,
+		// Don’t allow updates on Staging
+		'allowUpdates' => false,
+		// Prevent crawlers from indexing pages and following links
+		'disallowRobots' => true,
+	],
 
-    // Local environment settings
-    'local' => [
-        // Dev Mode (see https://craftcms.com/guides/what-dev-mode-does)
-        'devMode' => true,
-
-        // Prevent crawlers from indexing pages and following links
-        'disallowRobots' => true,
-    ],
-
-    // Dev environment settings
-    'dev' => [
-        // Dev Mode (see https://craftcms.com/guides/what-dev-mode-does)
-        'devMode' => true,
-
-        // Prevent crawlers from indexing pages and following links
-        'disallowRobots' => true,
-    ],
-
-    // Staging environment settings
-    'staging' => [
-        // Set this to `false` to prevent administrative changes from being made on Staging
-        'allowAdminChanges' => true,
-
-        // Don’t allow updates on Staging
-        'allowUpdates' => false,
-
-        // Prevent crawlers from indexing pages and following links
-        'disallowRobots' => true,
-    ],
-
-    // Production environment settings
-    'production' => [
-        // Set this to `false` to prevent administrative changes from being made on Production
-        'allowAdminChanges' => true,
-
-        // Don’t allow updates on Production
-        'allowUpdates' => false,
-    ],
+	// Production environment settings
+	'production' => [
+		// Set this to `false` to prevent administrative changes from being made on Production
+		'allowAdminChanges' => true,
+		// Don’t allow updates on Production
+		'allowUpdates' => false,
+	],
 ];
