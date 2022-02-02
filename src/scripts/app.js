222a485263a061bfc0523415b17accb25e3e5ce8
Derek Mohr <derek@mightyinthemidwest.com>
2022-02-02 17:47:18 -0500

    [#14] Rebuild scripts with Modujs environment

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,15 +1,50 @@
-/* ----------------------------------------
-  Import Components (ES6 Modules)
----------------------------------------- */
+// =============================================================================
+// App
+// =============================================================================
+// This file is the centerpiece of the javascript front end and kicks it all of
+// on load.
 
-import breadcrumb from './components/breadcrumb';
-import scroll from './components/scroll';
-import search from './components/search';
+// Import dependencies
+// =============================================================================
+import modular from 'modujs';
+import * as modules from './modules';
+import globals from './globals';
+import { html } from './utils/environment';
 
-/* ----------------------------------------
-  Initialize Components
----------------------------------------- */
+// Run a new modular instance
+// =============================================================================
+const app = new modular({
+  // Set to the modules imported above
+  modules: modules
+});
 
-breadcrumb();
-scroll();
-search();
+// Init our app
+// =========================================================================
+// Take the above 'Mighty Modular' instance, place it into our apps init and
+// start up our app!
+
+window.addEventListener('load', function () {
+  const $style = document.getElementById("stylesheet");
+
+  if ($style) {
+    init();
+  } else {
+    $style.addEventListener('load', (event) => {
+      init();
+    });
+  }
+});
+
+function init() {
+  // Delay site init
+  setTimeout(() => {
+    globals();
+    app.init(app);
+
+    html.classList.add('-is-loaded');
+    html.classList.add('-is-ready');
+    html.classList.remove('-is-loading');
+    html.classList.remove('-no-js');
+    html.classList.add('-has-js');
+  }, 300);
+}
---
+++ src/scripts/modules.js
@@ -0,0 +1,15 @@
+// =============================================================================
+// Modules
+// =============================================================================
+// Set all necessary modules here that we will need to watch for throughout the
+// front end.
+
+// Modules exports
+// =============================================================================
+export {default as Modal} from './modules/Modal';
+export {default as Navigation} from './modules/Navigation';
+export {default as Percentage} from './modules/Percentage';
+export {default as Scroll} from './modules/Scroll';
+export {default as Swiper} from './modules/Swiper';
+export {default as Testimonials} from './modules/Testimonials';
+export {default as Timeline} from './modules/Timeline';
